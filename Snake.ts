845f81e6c2358f984e88060409b980f743dfb321
alecradin <aradin@lions.molloy.edu>
2021-02-07 14:50:13 -0500

    2 files modified

--- Snake.ts
+++ Snake.ts
@@ -2,22 +2,50 @@ import display from './display';
 
 // place your code on line 5 above the export statement below
 
+class Point {
+  private xcoord: number;
+  private ycoord: number;
+  public constructor() {
+    this.xcoord = xcoord;
+    this.ycoord = ycoord;
+  }
+  public get nPoint() {
+  return this.xcoord,this.ycoord;
+  }
+}
 class Snake {
   private currentPosition: number;
   private currentDirection: number;
-  constructor () {
+  public constructor () {
     this.currentPosition = 0;
     this.currentDirection = 1;
   }
   public move(squares:number){
   this.currentPosition += this.currentDirection * squares;
+  if (this.position = newPoint(this.position.x,this.position.y)) return this.position 
   }
-  public turn(direction:number){
+  public turn(){
     this.currentDirection *= -1;
   }
   public get position() {
     return this.currentPosition;
   }
+  set newPoint (initialPoint:number) {
+    this.xcoord = 0;
+    this.ycoord = 0;
+  }
+  public turnLeft () {
+    if (this.xcoord < 0) display ("Left")
+    else if (this.ycoord < 0) display ("Down")
+    else if (this.xcoord > 0) display ("Right")
+    else display ("Up")
+  }
+  public turnRight () {
+    if (this.xcoord > 0) display ("Right")
+    else if (this.xoord < 0) display ("Left")
+    else if (this.ycoord < 0) display ("Down")
+    else display ("Up")
+  }
+  
 }
-
 export default Snake;
